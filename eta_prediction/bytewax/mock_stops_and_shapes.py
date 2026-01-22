7d65a1a9ab83b5bc107b4d4175b37cce26652622
Jae <dotjae609@gmail.com>
2026-01-21 18:05:27 -0600

    refactor: updated Redis port

--- eta_prediction/bytewax/mock_stops_and_shapes.py
+++ eta_prediction/bytewax/mock_stops_and_shapes.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
     try:
         client = redis.Redis(
             host="localhost",
-            port=6379,
+            port=6380,
             db=0,
             decode_responses=True
         )
